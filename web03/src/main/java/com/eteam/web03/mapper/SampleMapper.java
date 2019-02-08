22062f09d82a7df717e5cfac2491ee64cf3e3ab9
smart-09 <smart-09@smart-09-PC>
2019-02-08 14:21:01 +0900

    [최치원] sampleMapper.java 	   sampleMapper.xml 	       수정 및 작성 완료 - 쿼리문장 작성 (확인 해야함) - 주석 작성

--- web03/src/main/java/com/eteam/web03/mapper/SampleMapper.java
+++ web03/src/main/java/com/eteam/web03/mapper/SampleMapper.java
@@ -1,8 +1,12 @@
 package com.eteam.web03.mapper;
 
+import java.util.List;
+
 import org.apache.ibatis.annotations.Mapper;
+
+import com.eteam.web03.vo.Sample;
 
 @Mapper
 public interface SampleMapper {
-	
+	List<Sample> sampleList();
 }
